e4ee487d9cf61dc328f13efdfb5a5a07676e96ed
Quang Phạm <phamminhquang.work@gmail.com>
2023-09-14 15:00:04 +0700

    Xóa code thừa

--- lib/widget/cards/i_card.dart
+++ lib/widget/cards/i_card.dart
@@ -1,6 +1,1 @@
-import 'package:mannager_business/domains/models/products.dart';
-import 'package:mannager_business/widget/cards/base_cards.dart';
 
-abstract class Icard {
-  BaseCard products(Product product);
-}
